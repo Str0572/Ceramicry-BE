d52076301c5b23cbc5a80656ce4e08a5c6651743
Prashant Sharma <prashant100ji@gmail.com>
2025-10-11 13:55:12 +0530

    add product count

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,5 +1,5 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :mobile, :email, :status
+  attributes :id, :full_name, :mobile, :email, :status, :created_at, :updated_at
 
   has_many :addresses
 end
--- app/serializers/subcategory_serializer.rb
+++ app/serializers/subcategory_serializer.rb
@@ -1,5 +1,9 @@
 class SubcategorySerializer < ActiveModel::Serializer
-  attributes :id, :name, :slug, :description
+  attributes :id, :name, :slug, :description, :product_count
+
+  def product_count
+    object.products.count
+  end
 
   belongs_to :category
 end
