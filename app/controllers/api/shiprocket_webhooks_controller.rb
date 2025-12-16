3be0db99a1ba82dafcf804e6c93b0fdcd59b89b5
Prashant Sharma <prashant100ji@gmail.com>
2025-12-16 22:59:32 +0530

    fix error

--- app/controllers/api/shiprocket_webhooks_controller.rb
+++ app/controllers/api/shiprocket_webhooks_controller.rb
@@ -1,6 +1,6 @@
 module Api
-  class ShiprocketWebhooksController < ActionController::API
-    # skip_before_action :verify_authenticity_token
+  class ShiprocketWebhooksController < ActionController
+    skip_before_action :verify_authenticity_token
     skip_before_action :authenticate_request
 
     def receive
